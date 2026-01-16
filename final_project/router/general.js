7e0aa7f57e01cfe771f2d792662df0479b5c39e5
pedro-php <pedro.miranda@adaptweb.com.br>
2026-01-16 05:08:53 -0500

    async chanes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,37 +17,37 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).send(JSON.stringify(books))
+public_users.get('/',async function (req, res) {
+  return await new Promise(resolve => res.status(200).send(JSON.stringify(books)))
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    return res.status(200).send(JSON.stringify(books[req.params.isbn]))
+public_users.get('/isbn/:isbn',async function (req, res) {
+    return await new Promise(resolve => res.status(200).send(JSON.stringify(books[req.params.isbn])))
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
     const keys = Object.keys(books);
     let authors = {};
     for(key in books){
         if(books[key].author === req.params.author) authors[key] = books[key];
     }
-  return res.status(200).send(JSON.stringify(authors))
+  return await new Promise(resolve => res.status(200).send(JSON.stringify(authors)))
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async function (req, res) {
     const keys = Object.keys(books);
     let titles = {};
     for(key in books){
         if(books[key].title === req.params.title) titles[key] = books[key];
     }
-  return res.status(200).send(JSON.stringify(titles))
+  return await new Promise(resolve => res.status(200).send(JSON.stringify(titles)))
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn',async function (req, res) {
   
   return res.status(200).send(JSON.stringify(books[req.params.isbn].reviews));
 });
